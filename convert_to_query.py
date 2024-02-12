e226ea80ed51126f49fbd0aa1870b198a1b9fef8
MauricioDMO <mauriciodmoprogrammer@gmail.com>
2024-02-11 20:53:12 -0600

    Update column names in SQL queries

--- convert_to_query.py
+++ convert_to_query.py
@@ -5,15 +5,15 @@ municipality_id = 1
 district_id = 1
 
 for i, department in enumerate(ORGANIZATION_OBJECT):
-  TXT_FILE.write(f"INSERT INTO departamentos (id , name) VALUES ({i + 1}, '{' '.join([str.capitalize() for str in department.split(' ')])}');\n")
+  TXT_FILE.write(f"INSERT INTO departamentos (id , nombre) VALUES ({i + 1}, '{' '.join([str.capitalize() for str in department.split(' ')])}');\n")
   
   #? Municipios
   for j, municipality in enumerate(ORGANIZATION_OBJECT[department]):
-    TXT_FILE.write(f"INSERT INTO municipios (id , name, departamento_id) VALUES ({municipality_id}, '{' '.join([str.capitalize() for str in municipality.split(' ')])}', {i + 1});\n")
+    TXT_FILE.write(f"INSERT INTO municipios (id , nombre, departamento_id) VALUES ({municipality_id}, '{' '.join([str.capitalize() for str in municipality.split(' ')])}', {i + 1});\n")
     
     #? Distritos
     for k, district in enumerate(ORGANIZATION_OBJECT[department][municipality]):
-      TXT_FILE.write(f"INSERT INTO distritos (id , name, municipio_id) VALUES ({district_id}, '{' '.join([str.capitalize() for str in district.split(' ')])}', {municipality_id});\n")
+      TXT_FILE.write(f"INSERT INTO distritos (id , nombre, municipio_id) VALUES ({district_id}, '{' '.join([str.capitalize() for str in district.split(' ')])}', {municipality_id});\n")
       district_id += 1
 
     municipality_id += 1
